553027e2a11bbea513f37e6b779337c832e3c3fe
Myles MacRae <myles.macrae@xero.com>
2021-08-17 20:37:53 -0400

    repro project

---
+++ project/testproj/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace testproj
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine($"startup");
+        }
+        
+    }
+}
